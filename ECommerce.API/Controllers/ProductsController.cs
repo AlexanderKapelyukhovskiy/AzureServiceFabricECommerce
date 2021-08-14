bebd0422fd8ab1eae05a07386d9f3cf0164b79ec
Alex Kapelyukhovsky <alkapa2005@gmail.com>
2021-08-14 11:57:56 +0300

    Add project files.

---
+++ ECommerce.API/Controllers/ProductsController.cs
@@ -0,0 +1,55 @@
+﻿using ECommerce.API.Model;
+using ECommerce.ProductCatalog.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.ServiceFabric.Services.Client;
+using Microsoft.ServiceFabric.Services.Remoting.Client;
+using Microsoft.ServiceFabric.Services.Remoting.V2.FabricTransport.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECommerce.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductCatalogService _service;
+
+        public ProductsController()
+        {
+            var proxyFactory = new ServiceProxyFactory(c => new FabricTransportServiceRemotingClientFactory());
+            _service = proxyFactory.CreateServiceProxy<IProductCatalogService>(
+                new Uri("fabric:/ECommerce/ECommerce.ProductCatalog"), new ServicePartitionKey(0));
+        }
+        [HttpGet]
+        public async Task<IEnumerable<ApiProduct>> Get()
+        {
+            IEnumerable<Product> allProducts = await _service.GetAllProductsAsync();
+
+            return allProducts.Select(p => new ApiProduct
+            {
+                Id = p.Id,
+                Name = p.Name,
+                Description = p.Description,
+                Price = p.Price,
+                Available = p.Availability > 0
+            });
+        }
+
+        [HttpPost]
+        public async Task Post([FromBody] ApiProduct product)
+        {
+            var newProduct = new Product
+            {
+                Id = Guid.NewGuid(),
+                Name = product.Name,
+                Description = product.Description,
+                Price = product.Price,
+                Availability = 100
+            };
+            await _service.AddProductAsync(newProduct);
+        }
+    }
+}
---
+++ ECommerce.ProductCatalog/ServiceFabricProductRepository.cs
@@ -0,0 +1,51 @@
+﻿using ECommerce.ProductCatalog.Model;
+using Microsoft.ServiceFabric.Data;
+using Microsoft.ServiceFabric.Data.Collections;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ECommerce.ProductCatalog
+{
+    public class ServiceFabricProductRepository : IProductRepository
+    {
+        private readonly IReliableStateManager _stateManager;
+
+        public ServiceFabricProductRepository(IReliableStateManager stateManager)
+        {
+            _stateManager = stateManager;
+        }
+        public async Task AddProducts(Product product)
+        {
+            var products = await _stateManager.GetOrAddAsync<IReliableDictionary<Guid, Product>>("products");
+
+            using (ITransaction tx = _stateManager.CreateTransaction())
+            {
+                await products.AddOrUpdateAsync(tx, product.Id, product, (id, value) => product);
+                await tx.CommitAsync();
+            }
+        }
+
+        public async Task<IEnumerable<Product>> GetAllProducts()
+        {
+            var products = await _stateManager.GetOrAddAsync<IReliableDictionary<Guid, Product>>("products");
+            var result = new List<Product>();
+
+            using (ITransaction tx = _stateManager.CreateTransaction())
+            {
+                var allProducts = await products.CreateEnumerableAsync(tx, EnumerationMode.Unordered);
+
+                using (var enumberator = allProducts.GetAsyncEnumerator())
+                {
+                    while (await enumberator.MoveNextAsync(CancellationToken.None))
+                    {
+                        var current = enumberator.Current;
+                        result.Add(current.Value);
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
